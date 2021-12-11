30cff9570521f9e5d611f79a2c95e758f6abe37a
TitliChakraborty <93787434+TitliChakraborty@users.noreply.github.com>
2021-12-11 11:12:13 +0530

    Add files via upload

---
+++ loan_score_checker.py
@@ -0,0 +1,6 @@
+bank_balance=float(input(" Enter Your Bank Balance"))
+cibl_score=float(input(" 12Enter Your cibl_score"))
+if bank_balance>10000 or cibl_score>100:
+    print("You are eligible for Loan")
+else:
+    print("sorry! You are not eligible for Loan")
